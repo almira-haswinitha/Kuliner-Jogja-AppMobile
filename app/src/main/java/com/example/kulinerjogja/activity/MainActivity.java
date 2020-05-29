24fe51aef8ff5d2c47cecac70b8885e24cd73e45
najibroyyan <nroyyan56@gmail.com>
2020-05-29 08:53:45 +0700

    tampilan abstrak aplikasi

---
+++ app/src/main/java/com/example/kulinerjogja/activity/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.kulinerjogja.activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.kulinerjogja.R;
+import com.example.kulinerjogja.fragment.HomeFragment;
+import com.example.kulinerjogja.fragment.ListFragment;
+import com.example.kulinerjogja.fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //memunculkan fragment diawal
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmen_container,new HomeFragment())
+                .commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selected_fragment = null;
+            switch (item.getItemId()) {
+                case R.id.btnHome:
+                    selected_fragment = new HomeFragment();
+                    break;
+                case R.id.btnList:
+                    selected_fragment = new ListFragment();
+                    break;
+                case R.id.btnProfil:
+                    selected_fragment = new ProfileFragment();
+                    break;
+            }
+
+            assert selected_fragment != null;
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmen_container, selected_fragment)
+                    .commit();
+            return true;
+        }
+    };
+}
---
+++ app/src/main/java/com/example/kulinerjogja/fragment/ProfileFragment.java
@@ -0,0 +1,18 @@
+package com.example.kulinerjogja.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.kulinerjogja.R;
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+
+}
