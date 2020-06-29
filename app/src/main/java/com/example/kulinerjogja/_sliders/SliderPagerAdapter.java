35bd2b08657056add0fe08ca23834a594e56d91f
najibroyyan <nroyyan56@gmail.com>
2020-06-29 11:41:34 +0700

    penambahan image slider pada fragment home

---
+++ app/src/main/java/com/example/kulinerjogja/_sliders/SliderPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.kulinerjogja._sliders;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bagicode on 12/04/17.
+ */
+
+public class SliderPagerAdapter extends FragmentStatePagerAdapter {
+
+    private static final String TAG = "SliderPagerAdapter";
+
+    List<Fragment> mFrags = new ArrayList<>();
+
+    public SliderPagerAdapter(FragmentManager fm, List<Fragment> frags) {
+        super(fm);
+        mFrags = frags;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        int index = position % mFrags.size();
+        return FragmentSlider.newInstance(mFrags.get(index).getArguments().getString("params"));
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+
+}
--- app/src/main/java/com/example/kulinerjogja/fragment/HomeFragment.java
+++ app/src/main/java/com/example/kulinerjogja/fragment/HomeFragment.java
@@ -4,17 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.kulinerjogja.R;
+import com.example.kulinerjogja._sliders.FragmentSlider;
+import com.example.kulinerjogja._sliders.SliderIndicator;
+import com.example.kulinerjogja._sliders.SliderPagerAdapter;
+import com.example.kulinerjogja._sliders.SliderView;
 
 public class HomeFragment extends Fragment {
+    private SliderPagerAdapter mAdapter;
+    private SliderIndicator mIndicator;
+
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
+
+    public HomeFragment(){
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+        sliderView =(SliderView) rootView.findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) rootView.findViewById(R.id.pagesContainer);
+        setupSlider();
+
+        return rootView;
+    }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-1.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-2.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-3.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-4.jpg"));
+
+        mAdapter = new SliderPagerAdapter(getFragmentManager(), fragments);
+        sliderView.setAdapter(mAdapter);
+        mIndicator = new SliderIndicator(getActivity(), mLinearLayout, sliderView, R.drawable.indicator_circle);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
     }
 }
