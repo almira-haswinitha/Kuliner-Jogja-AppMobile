92cdb047a0a329cfd200e3be3549ee17b6007f0a
almira-haswinitha <almirahaswinitha@gmail.com>
2020-07-18 23:57:22 +0700

    menambah button sign in dan sign up

---
+++ app/src/main/java/com/example/kulinerjogja/config/ServerConfig.java
@@ -0,0 +1,7 @@
+package com.example.kulinerjogja.config;
+
+public class ServerConfig {
+    public static final String SERVER_URL = "http://10.0.2.2/kuliner_jogja/";
+    public static final String API_ENDPOINT = SERVER_URL;
+
+}
