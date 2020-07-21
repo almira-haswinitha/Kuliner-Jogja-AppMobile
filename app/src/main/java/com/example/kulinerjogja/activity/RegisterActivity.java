91d6e739c39259dd912b984d55d1ea66d6ac6279
almira-haswinitha <almirahaswinitha@gmail.com>
2020-07-21 14:22:52 +0700

    membuat dan menjalankan fungsi register

--- app/src/main/java/com/example/kulinerjogja/activity/RegisterActivity.java
+++ app/src/main/java/com/example/kulinerjogja/activity/RegisterActivity.java
@@ -2,8 +2,10 @@ package com.example.kulinerjogja.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.kulinerjogja.API.ApiClient;
 import com.example.kulinerjogja.R;
 import com.example.kulinerjogja.API.ApiInterface;
+import com.example.kulinerjogja.model.register.Register;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     EditText etUsername, etPassword, etNama, etEmail;
@@ -55,6 +62,27 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     }
 
     private void register(String username, String password, String nama, String email) {
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        Call<Register> call = apiInterface.registerResponse(username, password, nama, email);
+        call.enqueue(new Callback<Register>() {
+            @Override
+            public void onResponse(Call<Register> call, Response<Register> response) {
+                if(response.body() != null && response.isSuccessful() && response.body().isStatus()){
+                    Toast.makeText(RegisterActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(RegisterActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Register> call, Throwable t) {
+                Toast.makeText(RegisterActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
