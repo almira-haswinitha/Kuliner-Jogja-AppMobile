198d8d66ed89cf1f14853e794ef8fc0d18b3ef26
almira-haswinitha <almirahaswinitha@gmail.com>
2020-07-27 14:32:45 +0700

    siap menggunakan library yg ada

--- rest-server/application/config/config.php
+++ rest-server/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kuliner_jogja/rest_server/';
+$config['base_url'] = 'http://localhost/kuliner_jogja/rest-server/';
 
 /*
 |--------------------------------------------------------------------------
